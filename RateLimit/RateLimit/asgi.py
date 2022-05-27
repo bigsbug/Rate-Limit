64c631ddea0afb8dfa8b109581642780e09e2689
bigsbug <booxgitboox@gmail.com>
2022-05-28 02:52:07 +0430

    init project

---
+++ RateLimit/RateLimit/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RateLimit project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RateLimit.settings')
+
+application = get_asgi_application()
