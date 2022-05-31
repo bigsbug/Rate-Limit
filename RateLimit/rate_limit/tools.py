b0efcb260d132b488b652a50afdfd5e839e3bb05
bigsbug <booxbookboox@gmail.com>
2022-05-31 14:07:06 +0200

    add new tools and fix type return in git_ip

--- RateLimit/rate_limit/tools.py
+++ RateLimit/rate_limit/tools.py
@@ -1,14 +1,23 @@
+from typing import Any
 from django.http import HttpRequest
 
 
 def get_ip(request: HttpRequest) -> str:
     headers = request.headers
-
     if ip := headers.get("X-Forwarded-For", None):
         ip = str.strip(ip.split(",")[0])  # X-Forwarded-For can contain multi IP
     elif ip := headers.get("X-Real-IP", None):
         ...
     else:
-        return ""
+        return None
 
     return ip
+
+
+def settings_have(
+    settings, first_atter: str, second_atter: str = None, default: Any = None
+):
+    if second_atter:
+        DATA = getattr(settings, first_atter, {}).get(str(second_atter), default)
+        return DATA
+    return getattr(settings, str(first_atter), default)
