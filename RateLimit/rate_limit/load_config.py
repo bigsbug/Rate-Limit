07df4ee65ea986cd1e701badb94c49476bb45f69
bigsbug <booxgitboox@gmail.com>
2022-06-02 22:55:19 +0200

    refector and seperateing each loader

--- RateLimit/rate_limit/load_config.py
+++ RateLimit/rate_limit/load_config.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from rate_limit.exceptions import CantFindBackendRedis, InvalidConfig
 from rate_limit.tools import settings_have
-from load_config_interface import LoadConfigDB
+from rate_limit.load_config_interface import LoadConfigDB
 
 DEFAULT_KEY_PREFIX = "RATE_LIMIT"
 
 
-class LoadRedsiSettings(LoadConfigDB):
+class LoadRedisConfigFromRateLimit(LoadConfigDB):
     def __init__(self, settings) -> None:
         self.settings = settings
 
@@ -24,7 +24,50 @@ class LoadRedsiSettings(LoadConfigDB):
         """
         if redis_settings := settings_have(self.settings, "RATE_LIMIT", "REDIS"):
             ...
-        elif redis_settings := settings_have(self.settings, "CACHES", "default"):
+        else:
+            raise CantFindBackendRedis(
+                "configuration not found for Redis in settings please read the documents for more details in this [link]."
+            )
+
+        return redis_settings
+
+    def extract_config(self):
+        """extract redis configuration
+
+        Returns:
+            host : str
+                "redis://host:6379"
+            key_prefix : str
+                "RATE_LIMIT"
+        """
+        redis_settings = self.find_config()
+        key_prefix = redis_settings.get("KEY_PREFIX", DEFAULT_KEY_PREFIX)
+        if host := redis_settings.get("LOCATION", None):
+            ...
+        else:
+            raise InvalidConfig("Can't find LOCATION key in settings configuration")
+
+        return host, key_prefix
+
+
+class LoadRedisConfigFromCaches(LoadConfigDB):
+    def __init__(self, settings) -> None:
+        self.settings = settings
+
+    def find_config(self) -> dict:
+        """find redis configuration from settings
+
+        Raises:
+            CantFindBackendRedis: Can't find backend redis in settings
+
+        Returns:
+            dict: {
+                "LOCATOIN":"redis://host:6379",
+                "KEY_PREFIX":"RATE_LIMIT"
+            }
+        """
+
+        if redis_settings := settings_have(self.settings, "CACHES", "default"):
             if "RedisCache" not in redis_settings.get("BACKEND", ""):
                 raise CantFindBackendRedis(
                     "configuration not found for Redis in settings please read the documents for more details in this [link]."
@@ -45,7 +88,7 @@ class LoadRedsiSettings(LoadConfigDB):
             key_prefix : str
                 "RATE_LIMIT"
         """
-        redis_settings = self.find_redis_config()
+        redis_settings = self.find_config()
         key_prefix = redis_settings.get("KEY_PREFIX", DEFAULT_KEY_PREFIX)
         if host := redis_settings.get("LOCATION", None):
             ...
