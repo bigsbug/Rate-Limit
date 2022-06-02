4f383eca8342b8b000026fabf2f92efd20375c4d
bigsbug <booxgitboox@gmail.com>
2022-06-02 22:55:43 +0200

    refector and seperateing each loader test

--- RateLimit/rate_limit/tests/test_load_config.py
+++ RateLimit/rate_limit/tests/test_load_config.py
@@ -1,10 +1,88 @@
 from django.test import TestCase
-from rate_limit.load_config import DEFAULT_KEY_PREFIX, LoadRedsiSettings
+from rate_limit.load_config import (
+    DEFAULT_KEY_PREFIX,
+    LoadRedisConfigFromCaches,
+    LoadRedisConfigFromRateLimit,
+)
 from rate_limit.exceptions import CantFindBackendRedis, InvalidConfig
 
 
-class LoadRedsiSettings_FindRedisConfigTestCase(TestCase):
-    class SettingsWithCACHES:
+class LoadRedisConfigFromRateLimitTastCase(TestCase):
+    class Settings:
+        RATE_LIMIT = {
+            "REDIS": {
+                "LOCATION": "redis://127.0.0.1:6379",
+                "KEY_PREFIX": "example",
+            }
+        }
+
+    class InvalidSettings:
+        rate_limit = {
+            "redis": {
+                "location": "redis://127.0.0.1:6379",
+                "key_prefix": "example",
+            }
+        }
+
+    class SettingsWithoutKEY_PREFIX:
+        RATE_LIMIT = {
+            "REDIS": {
+                "LOCATION": "redis://127.0.0.1:6379",
+            }
+        }
+
+    class EmptySettings:
+        ...
+
+    def setUp(self) -> None:
+
+        self.settings_with_RATE_LIMIT = self.Settings()
+        self.valid_settings_without_KEY_PREFIX = self.SettingsWithoutKEY_PREFIX()
+        self.settings_with_invalid_RATE_LIMIT = self.InvalidSettings()
+        self.empty_settings = self.EmptySettings()
+
+    # Valid Config
+    def test_find_config_with_valid_settings(self):
+        result = LoadRedisConfigFromRateLimit(
+            self.settings_with_RATE_LIMIT
+        ).find_config()
+        excepted = {
+            "LOCATION": "redis://127.0.0.1:6379",
+            "KEY_PREFIX": "example",
+        }
+        self.assertEqual(result, excepted)
+
+    #  Invalid Config
+    def test_find_config_with_invalid_settings(self):
+        result = LoadRedisConfigFromRateLimit(
+            self.settings_with_invalid_RATE_LIMIT
+        ).find_config
+        excepted = CantFindBackendRedis
+        self.assertRaises(excepted, result)
+
+    def test_find_config_with_empty_settings(self):
+        result = LoadRedisConfigFromRateLimit(self.empty_settings).find_config
+        excepted = CantFindBackendRedis
+        self.assertRaises(excepted, result)
+
+    def test_extract_config_with_valid_settings(self):
+        excepted = excepted = ("redis://127.0.0.1:6379", "example")
+        result = LoadRedisConfigFromRateLimit(
+            self.settings_with_RATE_LIMIT
+        ).extract_config()
+        self.assertEqual(result, excepted)
+
+    def test_extract_config_without_KEY_PREFIX(self):
+
+        result = result = LoadRedisConfigFromRateLimit(
+            self.valid_settings_without_KEY_PREFIX
+        ).extract_config()
+        excepted = ("redis://127.0.0.1:6379", DEFAULT_KEY_PREFIX)
+        self.assertEqual(result, excepted)
+
+
+class LoadRedisConfigFromCachesTestCase(TestCase):
+    class Settings:
         CACHES = {
             "default": {
                 "BACKEND": "django.core.cache.backends.redis.RedisCache",
@@ -13,7 +91,15 @@ class LoadRedsiSettings_FindRedisConfigTestCase(TestCase):
             }
         }
 
-    class InvalidSettingsWithCACHES:
+    class SettingsWithoutKeyPrefix:
+        CACHES = {
+            "default": {
+                "BACKEND": "django.core.cache.backends.redis.RedisCache",
+                "LOCATION": "redis://127.0.0.1:6379",
+            }
+        }
+
+    class InvalidSettings:
         CACHES = {
             "default": {
                 "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
@@ -22,43 +108,18 @@ class LoadRedsiSettings_FindRedisConfigTestCase(TestCase):
             }
         }
 
-    class SettingsWithRATE_LIMIT:
-        RATE_LIMIT = {
-            "REDIS": {
-                "LOCATION": "redis://127.0.0.1:6379",
-                "KEY_PREFIX": "example",
-            }
-        }
-
-    class InvalidSettingsWithRATE_LIMIT:
-        rate_limit = {
-            "redis": {
-                "location": "redis://127.0.0.1:6379",
-                "key_prefix": "example",
-            }
-        }
-
     class EmptySettings:
         ...
 
     def setUp(self) -> None:
-        self.settings_with_CACHES = self.SettingsWithCACHES()
-        self.settings_with_invalid_CACHES = self.InvalidSettingsWithCACHES()
-        self.settings_with_RATE_LIMIT = self.SettingsWithRATE_LIMIT()
-        self.settings_with_invalid_RATE_LIMIT = self.InvalidSettingsWithRATE_LIMIT()
+        self.settings_with_CACHES = self.Settings()
+        self.settings_without_KEY_PREFIX = self.SettingsWithoutKeyPrefix()
+        self.settings_with_invalid_CACHES = self.InvalidSettings()
         self.empty_settings = self.EmptySettings()
 
     # Valid Config
-    def test_settings_with_RATE_LIMIT(self):
-        result = LoadRedsiSettings(self.settings_with_RATE_LIMIT).find_redis_config()
-        excepted = {
-            "LOCATION": "redis://127.0.0.1:6379",
-            "KEY_PREFIX": "example",
-        }
-        self.assertEqual(result, excepted)
-
-    def test_settings_with_CACHES(self):
-        result = LoadRedsiSettings(self.settings_with_CACHES).find_redis_config()
+    def test_find_config_with_valid_settings(self):
+        result = LoadRedisConfigFromCaches(self.settings_with_CACHES).find_config()
         excepted = {
             "BACKEND": "django.core.cache.backends.redis.RedisCache",
             "LOCATION": "redis://127.0.0.1:6379",
@@ -67,67 +128,27 @@ class LoadRedsiSettings_FindRedisConfigTestCase(TestCase):
         self.assertEqual(result, excepted)
 
     #  Invalid Config
-    def test_settings_with_invalid_RATE_LIMIT(self):
-        result = LoadRedsiSettings(
-            self.settings_with_invalid_RATE_LIMIT
-        ).find_redis_config
+    def test_find_config_with_invalid_settings(self):
+        result = LoadRedisConfigFromCaches(
+            self.settings_with_invalid_CACHES
+        ).find_config
         excepted = CantFindBackendRedis
         self.assertRaises(excepted, result)
 
-    def test_settings_with_invalid_CACHES(self):
-        result = LoadRedsiSettings(self.settings_with_invalid_CACHES).find_redis_config
+    def test_find_config_with_empty_settings(self):
+        result = LoadRedisConfigFromCaches(self.empty_settings).find_config
         excepted = CantFindBackendRedis
         self.assertRaises(excepted, result)
 
-    def test_empty_settings(self):
-        result = LoadRedsiSettings(self.empty_settings).find_redis_config
-        excepted = CantFindBackendRedis
-        self.assertRaises(excepted, result)
-
-
-class LoadRedsiSettings_ExtractConfigTestCase(TestCase):
-    DEFAULT_KEY_PREFIX = "RATE_LIMIT"
-
-    class SettingsWithCACHES:
-        CACHES = {
-            "default": {
-                "BACKEND": "django.core.cache.backends.redis.RedisCache",
-                "LOCATION": "redis://127.0.0.1:6379",
-                "KEY_PREFIX": "example",
-            }
-        }
-
-    class SettingsWithRATE_LIMIT:
-        RATE_LIMIT = {
-            "REDIS": {
-                "LOCATION": "redis://127.0.0.1:6379",
-                "KEY_PREFIX": "example",
-            }
-        }
-
-    class SettingsWithRATE_LIMITWithoutKEY_PREFIX:
-        RATE_LIMIT = {
-            "REDIS": {
-                "LOCATION": "redis://127.0.0.1:6379",
-            }
-        }
-
-    def setUp(self) -> None:
-        self.valid_settings = [self.SettingsWithCACHES(), self.SettingsWithRATE_LIMIT]
-        self.valid_settings_without_KEY_PREFIX = (
-            self.SettingsWithRATE_LIMITWithoutKEY_PREFIX()
-        )
-
-    def test_with_valid_settings(self):
+    def test_extract_config_with_valid_settings(self):
         excepted = excepted = ("redis://127.0.0.1:6379", "example")
-        for settings in self.valid_settings:
-            result = LoadRedsiSettings(settings).extract_config()
-            self.assertEqual(result, excepted)
+        result = LoadRedisConfigFromCaches(self.settings_with_CACHES).extract_config()
+        self.assertEqual(result, excepted)
 
-    def test_settings_with_RATE_LIMIT_and_without_KEY_PREFIX(self):
+    def test_extract_config_without_KEY_PREFIX(self):
 
-        result = result = LoadRedsiSettings(
-            self.valid_settings_without_KEY_PREFIX
+        result = result = LoadRedisConfigFromCaches(
+            self.settings_without_KEY_PREFIX
         ).extract_config()
-        excepted = ("redis://127.0.0.1:6379", self.DEFAULT_KEY_PREFIX)
+        excepted = ("redis://127.0.0.1:6379", DEFAULT_KEY_PREFIX)
         self.assertEqual(result, excepted)
