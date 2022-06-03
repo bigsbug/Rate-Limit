5b1132cde62eba5afc1add58504dff38b27ade79
bigsbug <booxgitboox@gmail.com>
2022-06-03 16:17:42 +0200

    fix git pointer and call DB functino for testing

--- RateLimit/rate_limit/decorators.py
+++ RateLimit/rate_limit/decorators.py
@@ -1,10 +1,8 @@
 from typing import Callable
 from django.http import HttpRequest
 from rate_limit.tools import get_ip
-<<<<<<< HEAD
-=======
-from rate_limit.load_config import show_settings
->>>>>>> 7b2fa517cc04c38737306ed35ca898f966cd4e95
+
+from rate_limit.db_manager import DB
 
 
 def rate_limit(max_rate=1):
@@ -12,16 +10,13 @@ def rate_limit(max_rate=1):
         def inner(request: HttpRequest, *args, **kwargs):
 
             ip = get_ip(request)
-<<<<<<< HEAD
             print(
                 f"The Decorator rate_limit is runed wiht max_rate {max_rate} for ip {ip}"
             )
-=======
-            show_settings()
+            DB()
             # print(
             #     f"The Decorator rate_limit is runed wiht max_rate {max_rate} for ip {ip}"
-            # )
->>>>>>> 7b2fa517cc04c38737306ed35ca898f966cd4e95
+
             return func(request, *args, **kwargs)
 
         return inner
