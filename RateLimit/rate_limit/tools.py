e5c5d51c3b1a02f0808bde035e516fce73192f40
bigsbug <booxgitboox@gmail.com>
2022-06-06 22:59:25 +0430

    create a new setting seracher and remove settings_have

--- RateLimit/rate_limit/tools.py
+++ RateLimit/rate_limit/tools.py
@@ -14,10 +14,46 @@ def get_ip(request: HttpRequest) -> str:
     return ip
 
 
-def settings_have(
-    settings, first_atter: str, second_atter: str = None, default: Any = None
-):
-    if second_atter:
-        DATA = getattr(settings, first_atter, {}).get(str(second_atter), default)
-        return DATA
-    return getattr(settings, str(first_atter), default)
+def lookup_setting(settings, settings_name):
+    """lookuping  the setting for attribute and sub-attribute
+
+    Args:
+        settings (class): django settings
+        settings_name (list,tuple): a list of attributes
+
+    Returns:
+        dict : value of the last attribute
+    """
+    config = None
+    if len(settings_name) > 1:
+        for config_name in settings_name:
+            if config is None:
+                config = getattr(settings, config_name, {})
+            else:
+                config = config.get(str(config_name), {})
+        return config
+    else:
+        return getattr(settings, str(settings_name[0]), {})
+
+
+class Settings:
+    RATE_LIMIT = {
+        "REDIS": {
+            "LOCATION": "redis://redis-db:6379/1",
+        },
+        "RATE": {
+            "user": "1000/day",
+            "ip": "10/min",
+            "anonymous": "100/day",
+        },
+    }
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis-db:6379/1",
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+        "KEY_PREFIX": "example",
+    }
+}
