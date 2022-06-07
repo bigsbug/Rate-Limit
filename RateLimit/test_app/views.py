1fea5578595f32a4a5869e32d430dc6548dffe4d
bigsbug <booxgitboox@gmail.com>
2022-06-08 02:31:53 +0430

    remove unless imports and active new limiter on index view

--- RateLimit/test_app/views.py
+++ RateLimit/test_app/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpRequest, HttpResponse
 from rate_limit.decorators import rate_limit
+from rate_limit.limiters import RateLimitAnonymous, RateLimitUser, RateLimitIP
+
+from rate_limit.db_manager import (
+    LoadRedisConfigFromRateLimit,
+    PerformActionRedis,
+    Redis,
+    settings,
+)
+
+myredis = Redis()
+redis_config = LoadRedisConfigFromRateLimit(settings)
+myredis.load_config(redis_config)
+myredis.connect()
+performaction = PerformActionRedis()
+performaction.set_db(myredis)
 
 
-@rate_limit(max_rate=10)
-def index(request):
-    return HttpResponse("you successfully access to site")
+@rate_limit([RateLimitAnonymous()])
+def index(request: HttpRequest):
+    count = request.count_hit
+
+    return HttpResponse(f"user successfully access to site {count} times")
