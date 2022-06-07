6980d6f7d92b740a8fa99a4dc72bb68ffc654fac
bigsbug <booxgitboox@gmail.com>
2022-06-08 02:32:47 +0430

    remove unless imports and database connector

--- RateLimit/test_app/views.py
+++ RateLimit/test_app/views.py
@@ -2,20 +2,6 @@ from django.http import HttpRequest, HttpResponse
 from rate_limit.decorators import rate_limit
 from rate_limit.limiters import RateLimitAnonymous, RateLimitUser, RateLimitIP
 
-from rate_limit.db_manager import (
-    LoadRedisConfigFromRateLimit,
-    PerformActionRedis,
-    Redis,
-    settings,
-)
-
-myredis = Redis()
-redis_config = LoadRedisConfigFromRateLimit(settings)
-myredis.load_config(redis_config)
-myredis.connect()
-performaction = PerformActionRedis()
-performaction.set_db(myredis)
-
 
 @rate_limit([RateLimitAnonymous()])
 def index(request: HttpRequest):
