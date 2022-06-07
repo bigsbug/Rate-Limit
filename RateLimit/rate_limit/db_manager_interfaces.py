df808be1d62285fa90504fb1e0b11b0f2653e52c
bigsbug <booxgitboox@gmail.com>
2022-06-08 02:35:57 +0430

    create new interface for intracting with DBs

--- RateLimit/rate_limit/db_manager_interfaces.py
+++ RateLimit/rate_limit/db_manager_interfaces.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractclassmethod
-from rate_limit.load_config_interface import LoadConfigDB
+from rate_limit.load_config_interface import ConfigLoderInterface
 
 
 class DBManagerInterface(ABC):
@@ -8,11 +8,57 @@ class DBManagerInterface(ABC):
 
     def close(self):
         ...
-        
+
     def load_config(self):
         ...
 
     def instance(self):
+        ...
+
+
+class PerformActionDBInterface(ABC):
+    @abstractclassmethod
+    def set_db(self, DB: DBManagerInterface) -> None:
+        ...
+
+    @abstractclassmethod
+    def set_expire(self, key, value):
+        ...
+
+    @abstractclassmethod
+    def show_ttl(self, key):
+        ...
+
+    @abstractclassmethod
+    def new_view(self, key) -> str:
+        ...
+
+    @abstractclassmethod
+    def show_view(self, key) -> str:
+        ...
+
+    @abstractclassmethod
+    def add_to_whitelist(self, target) -> bool:
+        ...
+
+    @abstractclassmethod
+    def is_in_whitelist(self, target) -> bool:
+        ...
+
+    @abstractclassmethod
+    def remove_from_whitelist(self, target) -> bool:
+        ...
+
+    @abstractclassmethod
+    def add_to_blacklist(self, target) -> bool:
+        ...
+
+    @abstractclassmethod
+    def is_in_blacklist(self, target) -> bool:
+        ...
+
+    @abstractclassmethod
+    def remove_from_blacklist(self, target) -> bool:
         ...
 
 
