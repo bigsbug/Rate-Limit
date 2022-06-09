3a241dcd8e0a755afbe36ed6b4dfa157896ab4b5
bigsbug <booxgitboox@gmail.com>
2022-06-09 04:50:19 +0430

    Docuemnt CodeBase

--- RateLimit/rate_limit/limiters.py
+++ RateLimit/rate_limit/limiters.py
@@ -1,3 +1,4 @@
+from rate_limit.exceptions import InvalidConfig
 from rate_limit.db_manager_interfaces import RateLimiterInterface
 
 from rate_limit.load_config import LimitBy, LoadKeyPerfix, TimeLimit
@@ -5,11 +6,18 @@ from rate_limit.load_config import LimitBy, LoadKeyPerfix, TimeLimit
 from django.conf import settings
 
 from rate_limit.tools import get_ip
+from rate_limit.db_manager_interfaces import PerformActionDBInterface
 
 
 class BaseRateLimit(RateLimiterInterface):
-    def __init__(self, max_rate=None, time="min", type_limit="") -> None:
+    """Base Class For Creating New Limiters,
+    Providing Some Of Common Base Method For Limiters
 
+    Args:
+        RateLimiterInterface (interface): Interface of Limiters
+    """
+
+    def __init__(self, max_rate=None, time="min", type_limit="") -> None:
         self.type_limit = type_limit
         self.load_config()
         #  if specify a new rate limit then should select a time for limiting access
@@ -18,19 +26,39 @@ class BaseRateLimit(RateLimiterInterface):
             self.time = time
 
     def load_config(self):
+        """Load Max Rate and Time Limit From Settings"""
         self.max_rate_count, self.time = LimitBy(
             settings, [self.type_limit]
         ).extract_config()
         self.max_rate_count = int(self.max_rate_count)
 
-    def set_db(self, performer_action_db):
+    def set_db(self, performer_action_db: PerformActionDBInterface):
+        """Setting A Performer DB To Interacting With DataBase
+
+        Args:
+            performer_action_db (PerformActionDBInterface): get a Perform Action DB Interface instance
+        """
         self.performer_action_db = performer_action_db
 
     @property
     def max_rate(self):
+        """Return Max Rate Allowd For Limiter
+
+        Returns:
+            Int : Max Rate Allowd For This Limiter
+        """
         return self.max_rate_count
 
     def expireing(self, key):
+        """Expiring A Unique Key From Database After A Period Time Based Specified Time In Settings
+
+        Args:
+            key (Any): Unique Key For Detecting Which Data Should Be Expire
+
+        Raises:
+            InvalidConfig: The Time Limited in Settings Specified Is Incorrect
+        """
+
         try:
             time = TimeLimit[str.upper(self.time)].value
         except KeyError:
